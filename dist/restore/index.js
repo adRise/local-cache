c1ce310ea04c3d6c684b285c05c3a871da4d1293
tomchang-tubi <lchang@tubi.tv>
2025-08-14 00:22:42 +0800

    fix: use seprated touch cmd

--- dist/restore/index.js
+++ dist/restore/index.js
@@ -3976,11 +3976,12 @@ function run() {
             core.saveState('cache-hit', String(cacheHit));
             core.setOutput('cache-hit', String(cacheHit));
             if (cacheHit === true) {
-                const ln = yield (0, cache_1.exec)(`ln -s ${p.join(cachePath, path.split('/').slice(-1)[0])} ./${path} && touch -a ./${path}`);
+                const ln = yield (0, cache_1.exec)(`ln -s ${p.join(cachePath, path.split('/').slice(-1)[0])} ./${path}`);
                 core.debug(ln.stdout);
                 if (ln.stderr)
                     core.error(ln.stderr);
                 if (!ln.stderr)
+                    yield (0, cache_1.exec)(`touch ${p.join(cachePath, path.split('/').slice(-1)[0])}`);
                     core.info(`Cache restored with key ${key}`);
             }
             else {
